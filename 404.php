1a05614c59e0ea0e7f4efb1d41d2e9d5d2bab02c
Iryna <Iryna@MacBook-Air-MacBook.local>
2024-05-02 22:57:20 +0300

    add starter files

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package wp-it-volunteers
+ */
+
+get_header();
+?>
+
+	<main id="primary">
+		<section>
+		</section>
+	</main>
+
+<?php get_footer(); ?>
