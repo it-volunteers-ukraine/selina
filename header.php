4621cf9924520b044c01bf9be42e9b1331ff7eb2
kuharenko-olya <olyavolk49@gmail.com>
2024-05-27 15:56:49 +0300

    fix black-logo

--- header.php
+++ header.php
@@ -24,7 +24,19 @@
                 </div>
 
                 <div class="black-logo">
-                    <img class="logo" src="<?php echo get_template_directory_uri() . '/src/images/logo_icon.svg'; ?>" alt="black-logo">
+                    <div class="logo">
+                        <?php
+                        if (has_custom_logo()) {
+                            $custom_logo = get_custom_logo();
+                            if (preg_match('/<img[^>]+src=["\']([^"\']+)["\']/', $custom_logo, $matches)) {
+                                $original_logo_src = $matches[1];
+                                $black_logo_src = 'https://selina.it-volunteers.com/wp-content/themes/selina/src/images/logo_icon.svg';
+                                $updated_logo = str_replace($original_logo_src, $black_logo_src, $custom_logo);
+                                echo $updated_logo;
+                            }
+                        }
+                        ?>
+                    </div>
                 </div>
 
                 <div class="wrapper-buttons">
@@ -62,13 +74,13 @@
                         }
                         ?>
                     </div>
-                    <div class="burger">
+                    <button class="burger">
                         <svg width="44" height="28" viewBox="0 0 44 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd"
                                   d="M0 2.3125C0 1.76549 0.217298 1.24089 0.604092 0.854092C0.990886 0.467298 1.51549 0.25 2.0625 0.25H41.9375C42.4845 0.25 43.0091 0.467298 43.3959 0.854092C43.7827 1.24089 44 1.76549 44 2.3125C44 2.85951 43.7827 3.38411 43.3959 3.77091C43.0091 4.1577 42.4845 4.375 41.9375 4.375H2.0625C1.51549 4.375 0.990886 4.1577 0.604092 3.77091C0.217298 3.38411 0 2.85951 0 2.3125ZM0 14C0 13.453 0.217298 12.9284 0.604092 12.5416C0.990886 12.1548 1.51549 11.9375 2.0625 11.9375H41.9375C42.4845 11.9375 43.0091 12.1548 43.3959 12.5416C43.7827 12.9284 44 13.453 44 14C44 14.547 43.7827 15.0716 43.3959 15.4584C43.0091 15.8452 42.4845 16.0625 41.9375 16.0625H2.0625C1.51549 16.0625 0.990886 15.8452 0.604092 15.4584C0.217298 15.0716 0 14.547 0 14ZM2.0625 23.625C1.51549 23.625 0.990886 23.8423 0.604092 24.2291C0.217298 24.6159 0 25.1405 0 25.6875C0 26.2345 0.217298 26.7591 0.604092 27.1459C0.990886 27.5327 1.51549 27.75 2.0625 27.75H41.9375C42.4845 27.75 43.0091 27.5327 43.3959 27.1459C43.7827 26.7591 44 26.2345 44 25.6875C44 25.1405 43.7827 24.6159 43.3959 24.2291C43.0091 23.8423 42.4845 23.625 41.9375 23.625H2.0625Z"
                                   fill="white"/>
                         </svg>
-                    </div>
+                    </button>
                 </div>
                 <div class="wrapper-nav">
                     <nav class="nav">
