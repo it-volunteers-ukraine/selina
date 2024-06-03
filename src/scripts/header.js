281a830db51c91a217204e200d673bace98bf4de
kuharenko-olya <olyavolk49@gmail.com>
2024-06-03 19:11:24 +0300

    add burger menu and toggle switch

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,14 +1,21 @@
 // Burger menu
 
-const toggleMenu = document.getElementById('header-toggle-menu');
-const closeButton = document.getElementById('header-close-btn');
 const navMenu = document.getElementById('header-nav-menu');
+const burgerBtn = document.getElementById('header-menu-btn')
+const headerTopMobile = document.getElementById('header-mobile');
 
-toggleMenu.addEventListener('click', () => {
-    navMenu.classList.add('mobile-menu')
-    navMenu.classList.add('show-menu');
+
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('open');
+    navMenu.classList.toggle('mobile-menu');
+    navMenu.classList.toggle('show-menu');
+    headerTopMobile.classList.toggle('header-mobile');
 })
 
-closeButton.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-})
+
+// for polylang
+function redirectToPage(url) {
+    window.location.href = url;
+}
+
+
