affcbafe525ced0bf1375390f7101259281606b1
kuharenko-olya <olyavolk49@gmail.com>
2024-06-06 17:04:18 +0300

    fix styles and add dropdowns

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -4,6 +4,10 @@ const navMenu = document.getElementById('header-nav-menu');
 const burgerBtn = document.getElementById('header-menu-btn')
 const headerTopMobile = document.getElementById('header-mobile');
 
+// dropdowns
+const headerList = document.querySelector('.header__list');
+const itemsList = headerList.querySelectorAll('.menu-item');
+
 
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('open');
@@ -19,3 +23,14 @@ function redirectToPage(url) {
 }
 
 
+itemsList.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.add('active');
+        if (item.classList.contains('active')) {
+            const submenu = item.querySelector('.sub-menu');
+            if (submenu) {
+                submenu.classList.toggle('active-sub_menu');
+            }
+        }
+    })
+})
