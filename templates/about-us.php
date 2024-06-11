72c9fb394f2bd46e0ad8d33e2d3990031783fdd5
Oksana Baranik <ksyubaranik@gmail.com>
2024-06-11 07:52:42 +0200

    Created files for about-us page

---
+++ templates/about-us.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: about-us
+*/
+get_header();
+?>
+<main class="about-us">
+    <section class="about-us__list section" id="about-list">
+        <div class="container">
+
+        </div>
+    </section>
+    <section class="about-us__club section" id="about-club">
+        <div class="container">
+
+        </div>
+    </section>
+    <section class="about-us__map section" id="about-map">
+        <div class="container">
+
+        </div>
+    </section>
+    <section class="about-us__management section" id="about-management">
+        <div class="container">
+
+        </div>
+    </section>
+    <section class="about-us__documents section" id="about-documents">
+        <div class="container">
+
+        </div>
+    </section>
+    <section class="about-us__feedback section" id="about-feedback">
+        <div class="container">
+
+        </div>
+    </section>
+</main>
