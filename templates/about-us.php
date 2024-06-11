20d469dd7035518b8d743144119b44235853d80f
Oksana Baranik <ksyubaranik@gmail.com>
2024-06-11 09:51:32 +0200

    Added content to club-section

--- templates/about-us.php
+++ templates/about-us.php
@@ -5,32 +5,62 @@ Template Name: about-us
 get_header();
 ?>
 <main class="about-us">
-    <section class="about-us__list section" id="about-list">
+    <section class="list-section section" id="about-list">
+        <div class="list-section__background" style="background-image: url(<?php the_field('list-section__background', 'option') ?>);">
+            <div class="container">
+
+            </div>
+        </div>
+    </section>
+    <section class="club-section section" id="about-club">
+        <div class="container">
+            <h2 class="club-section__heading section_heading">
+                <svg class="club-heading-svg" width="29" height="28">
+                    <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-La_cat">
+                    </use>
+                </svg>
+                <?php the_field('club-section__heading'); ?>
+            </h2>
+            <div class="club-section__wrapper">
+                <div class="club-img-1">
+                    <?php 
+                        $image = get_field('club-img-1');
+                        if( !empty( $image ) ): ?>
+                            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"/>
+                    <?php endif; ?>
+                </div>
+                <div class="club-info-1">
+                    <p><?php the_field('club-info-1'); ?></p>
+                </div>
+                <div class="club-img-2">
+                    <?php 
+                        $image = get_field('club-img-2');
+                        if( !empty( $image ) ): ?>
+                            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"/>
+                    <?php endif; ?>
+                </div>
+                <div class="club-info-2">
+                    <p><?php the_field('club-info-2'); ?></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="map-section section" id="about-map">
         <div class="container">
 
         </div>
     </section>
-    <section class="about-us__club section" id="about-club">
+    <section class="management-section section" id="about-management">
         <div class="container">
 
         </div>
     </section>
-    <section class="about-us__map section" id="about-map">
+    <section class="documents-section section" id="about-documents">
         <div class="container">
 
         </div>
     </section>
-    <section class="about-us__management section" id="about-management">
-        <div class="container">
-
-        </div>
-    </section>
-    <section class="about-us__documents section" id="about-documents">
-        <div class="container">
-
-        </div>
-    </section>
-    <section class="about-us__feedback section" id="about-feedback">
+    <section class="feedback-section section" id="about-feedback">
         <div class="container">
 
         </div>
