c35a04a230e0aae8a14522aaf861facaa0edb284
kuharenko-olya <olyavolk49@gmail.com>
2024-06-27 17:01:10 +0300

    fix sub-menu

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -4,13 +4,15 @@ const navMenu = document.getElementById('header-nav-menu');
 const burgerBtn = document.getElementById('header-menu-btn')
 const headerTopMobile = document.getElementById('header-mobile');
 
-// dropdowns
-const headerList = document.querySelector('.header__list');
-const itemsList = headerList.querySelectorAll('.menu-item');
 
 // submenu - mobile
 const headerListMobile = document.querySelector('.header__list-mobile');
 const itemsListMobile = headerListMobile.querySelectorAll('.menu-item');
+
+
+// dropdowns
+const headerSubmenus = document.querySelectorAll('header .sub-menu');
+const headerMenuList = document.querySelectorAll('header .header__list .menu-item');
 
 
 burgerBtn.addEventListener('click', () => {
@@ -27,9 +29,23 @@ function redirectToPage(url) {
 }
 
 
-itemsList.forEach(item => {
-    item.addEventListener('click', (event) => {
-        const subMenu = item.querySelector('.sub-menu');
+// remove global class ul.sub-menu
+headerSubmenus.forEach(innerMenu => {
+    innerMenu.classList.remove('sub-menu');
+    innerMenu.classList.add('header-sub-menu');
+})
+
+// remove global class li.menu-item
+headerMenuList.forEach(menuItem => {
+    menuItem.classList.remove('menu-item');
+    menuItem.classList.add('header-menu-item');
+})
+
+
+headerMenuList.forEach(item => {
+    item.addEventListener('click', () => {
+        const subMenu = item.querySelector('.header-sub-menu');
+
         if (subMenu) {
             subMenu.classList.toggle('active-sub_menu');
         }
@@ -38,7 +54,7 @@ itemsList.forEach(item => {
 
 
 itemsListMobile.forEach(item => {
-    item.addEventListener('click', (event) => {
+    item.addEventListener('click', () => {
         const subMenu = item.querySelector('.sub-menu');
         if (subMenu) {
             subMenu.classList.toggle('active-sub_menu-mobile');
