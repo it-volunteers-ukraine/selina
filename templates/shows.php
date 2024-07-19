c7f7eb3eec40c62a98aea5e186855dc1d8cc32ab
hettfield git <olekpl90@gmail.com>
2024-07-19 14:12:08 +0200

    done basic markup

--- templates/shows.php
+++ templates/shows.php
@@ -48,6 +48,65 @@ get_header();
             </div>
         </section>
 
+        <section class='section webinars' id='webinars'>
+            <div class="container">
+                <div class="webinars__heading-container">
+                    <span class="list-webinars__La_cat"> 
+                        <svg width="24" height="24"> 
+                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/sprite.svg#icon-La_cat"></use> 
+                        </svg> 
+                    </span> 
+                    <h2 class="webinars__heading">
+                        <?php the_field('webinars__heading'); ?>
+                    </h2>
+                </div>
+                <p class=webinars__description>
+                    <?php the_field('webinars__description'); ?>
+                </p>
+                <div class="webinars__card"></div>
+                <a href="">
+                    <div class="webinars__last-button">
+                        <p class=webinars__last-button-text>
+                            <?php the_field('webinars__last-button-text'); ?>
+                        </p>
+                        <svg class="icon-paw" width="17" height="15">
+                            <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-paw">
+                            </use>
+                        </svg>
+                    </div>
+                </a>
+            </div>
+        </section>
+
+        <section class='section presentations' id='presentations'>
+            <div class="container">
+                <div class="presentations__heading-container">
+                    <span class="list-presentations__La_cat"> 
+                        <svg width="24" height="24"> 
+                            <use href="<?php bloginfo( 'template_url' ); ?>/assets/images/sprite.svg#icon-La_cat"></use> 
+                        </svg> 
+                    </span> 
+                    <h2 class="presentations__heading">
+                        <?php the_field('presentations__heading'); ?>
+                    </h2>
+                </div>
+                <div class="presentations__card"></div>
+                <div class="presentations__card"></div>
+                <div class="presentations__card"></div>
+                <div class="presentations__card"></div>
+                <a href="">
+                    <div class="webinars__last-button">
+                        <p class=webinars__last-button-text>
+                            <?php the_field('webinars__last-button-text'); ?>
+                        </p>
+                        <svg class="icon-paw" width="17" height="15">
+                            <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-paw">
+                            </use>
+                        </svg>
+                    </div>
+                </a>
+            </div>
+        </section>
         
         <?php get_template_part('template-parts/join-us') ?>
     </main>
