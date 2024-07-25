10d4673ae868fc04ab5016be86a4c8a878eea69e
Iryna <Iryna@MacBook-Air.local>
2024-07-25 23:58:20 +0300

    pagination breeders ready

--- src/scripts/template-scripts/single-breeders.js
+++ src/scripts/template-scripts/single-breeders.js
@@ -1,52 +1,25 @@
-jQuery(document).on("ready", function () {
-  const swiper1List = Array.from(
-    document.querySelector(".single-breeder-our-cats-section__list")
-      ?.children ?? []
-  );
-  const a = document.querySelector("#slick-slider-1");
-  const swiper2List = [
-    ...swiper1List.slice(2).map((n) => n.cloneNode(true)),
-    ...swiper1List.slice(0, 2).map((n) => n.cloneNode(true)),
-  ];
-  const swiper3List = [
-    ...swiper1List.slice(3).map((n) => n.cloneNode(true)),
-    ...swiper1List.slice(0, 3).map((n) => n.cloneNode(true)),
-  ];
-  const swiper1Container = document.querySelector("#slick-slider-1");
-  const swiper2Container = document.querySelector("#slick-slider-2");
-  const swiper3Container = document.querySelector("#slick-slider-3");
-
-  swiper1Container.append(...swiper1List);
-  swiper2Container.append(...swiper2List);
-  swiper3Container.append(...swiper3List);
-
-  jQuery("#slick-slider-1").slick({
-    vertical: true,
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    asNavFor: "#slick-slider-2,#slick-slider-3",
-  });
-  jQuery("#slick-slider-2").slick({
-    horizontal: true,
-    dots: false,
-    arrows: true,
-    infinite: true,
-    fade: true,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: "#slick-slider-1,#slick-slider-3",
-  });
-  jQuery("#slick-slider-3").slick({
-    vertical: true,
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    asNavFor: "#slick-slider-1,#slick-slider-2",
-  });
+const newsSwiper = new Swiper(".single-breeder-our-cats-section__swiper", {
+  effect: "slide",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 24,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1439: {
+      slidesPerView: 3,
+    },
+  },
+  direction: "horizontal",
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: true,
+    loadPrevNext: true,
+  },
+  lazyPreloadPrevNext: 1,
+  navigation: {
+    nextEl: ".single-breeder-our-cats-section__arrow-right-btn",
+    prevEl: ".single-breeder-our-cats-section__arrow-left-btn",
+  },
 });
