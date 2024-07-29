0c0f9cf187e79d1ddac28f8ee72560cd2a15cf7a
Oksana Baranik <ksyubaranik@gmail.com>
2024-07-29 17:51:13 +0200

    Made club Rules section, made transparent_medium_button in global.scss

--- templates/club-membership.php
+++ templates/club-membership.php
@@ -6,7 +6,7 @@ get_header();
 ?>
 
 <main class="club-membership">
-<section class="upper-section section" id="club-membership-upper">
+    <section class="upper-section section" id="club-membership-upper">
         <style>
             @media screen and (min-width: 1439px) {
                 .upper-section {
@@ -51,6 +51,56 @@ get_header();
                 </div> 
             </div>
         </div>    
+    </section>
+
+    <!-- Club Rules section ------------------------------------------>
+    <section class="club-rules-section section" id="membership-club-rules">
+        <div class="container">
+            <div class="club-rules-section__wrapper">
+                <h2 class="club-rules-section__heading section_heading club-rules-sections-heading">
+                    <svg class="heading-svg" width="29" height="28">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-La_cat">
+                        </use>
+                    </svg>
+                    <?php the_field('club-rules-section__heading'); ?>
+                </h2>
+                <div class="club-rules-section__content">
+                    <div class="club-rules-section__upper-card">
+                        <img src="<?php the_field('club-rules-img'); ?>" />
+                    </div>
+                    <div class="club-rules-section__lower-card">
+                        <div class="club-rules-section__lower-card-wrapper">
+                            <p class="club-rules-section__lower-card-title">
+                                <?php the_field('club-rules-section__lower-card-title'); ?>
+                            </p>
+                            <?php if( have_rows('club-rules-section__lower-card-list') ): ?>
+                                <ul class="club-rules-section__lower-card-list">
+                                    <?php while( have_rows('club-rules-section__lower-card-list') ): the_row(); 
+                                        $clubMembershipRule = get_sub_field('club-membership-rule');
+                                        ?>
+                                        <li>
+                                            <?php echo $clubMembershipRule; ?>
+                                        </li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            <?php endif; ?>
+                        </div>
+                        <p class="club-rules-section__lower-card-text">
+                            <?php the_field('club-rules-section__lower-card-text'); ?>
+                        </p>
+                        <button class="club-rules-section__lower-card-button transparent_medium_button">
+                            <a href="<?php echo esc_attr( get_field('club-rules-section__link') ); ?>" target="_blank">
+                                <?php the_field('club-rules-section__btn'); ?>
+                            </a> 
+                            <svg class="about_wcf-section__button-svg" width="20" height="20">
+                                <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow-up-right"></use>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     </section>
 </main>
 
