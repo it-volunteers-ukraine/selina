bdaa26f99f739dbc5167d3bc616c3e9c4e301c69
hettfield git <olekpl90@gmail.com>
2024-08-01 15:59:36 +0200

    top btn in fixed position. form submit-button not active while checkbox not checked

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -1,42 +1,42 @@
 // Scroll to TOP
 let scrollTopBtn = document.getElementById("to-top-btn");
 
-window.onscroll = function() {
-    scrollFunction();
-};
+// window.onscroll = function() {
+//     scrollFunction();
+// };
 
-function scrollFunction() {
-    let windowHeight = window.innerHeight;
-    let documentHeight = document.documentElement.scrollHeight;
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    let bottomDistance = documentHeight - (scrollTop + windowHeight);
+// function scrollFunction() {
+//     let windowHeight = window.innerHeight;
+//     let documentHeight = document.documentElement.scrollHeight;
+//     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     let bottomDistance = documentHeight - (scrollTop + windowHeight);
 
-    if (scrollTop > 20) {
-        scrollTopBtn.style.display = "block";
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
+//     if (scrollTop < 20) {
+//         scrollTopBtn.style.display = "none";
+//     } else {
+//         scrollTopBtn.style.display = "block";
+//     }
 
-    if (window.innerWidth <= 767) {
-        if (bottomDistance <= 291) {
-            scrollTopBtn.style.bottom = `${291-bottomDistance + 16}px`;
-        } else {
-            scrollTopBtn.style.bottom = '16px';
-        }
-    } else if (window.innerWidth <= 1439) {
-        if (bottomDistance <= 401) {
-            scrollTopBtn.style.bottom = `${401-bottomDistance + 16}px`;
-        } else {
-            scrollTopBtn.style.bottom = '16px';
-        }
-    } else {
-        if (bottomDistance <= 430) {
-            scrollTopBtn.style.bottom = `${430-bottomDistance + 16}px`;
-        } else {
-            scrollTopBtn.style.bottom = '16px';
-        }
-    }
-}
+//     if (window.innerWidth <= 767) {
+//         if (bottomDistance <= 291) {
+//             scrollTopBtn.style.bottom = `${291-bottomDistance + 16}px`;
+//         } else {
+//             scrollTopBtn.style.bottom = '16px';
+//         }
+//     } else if (window.innerWidth <= 1438) {
+//         if (bottomDistance <= 401) {
+//             scrollTopBtn.style.bottom = `${401-bottomDistance + 16}px`;
+//         } else {
+//             scrollTopBtn.style.bottom = '16px';
+//         }
+//     } else {
+//         if (bottomDistance <= 430) {
+//             scrollTopBtn.style.bottom = `${430-bottomDistance + 16}px`;
+//         } else {
+//             scrollTopBtn.style.bottom = '16px';
+//         }
+//     }
+// }
 
 scrollTopBtn.addEventListener('click', function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
