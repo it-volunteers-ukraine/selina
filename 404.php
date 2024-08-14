60b11fd7e53dc0d36a81212ecad782a716c24c5f
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-08-14 11:10:05 +0300

    Added acf fields to 404 not found page.

--- 404.php
+++ 404.php
@@ -15,14 +15,17 @@ get_header();
 			<div class="container">
 				<div class="not-found-content">
 					<div class="not-found-content__img-part">
-						<!-- <img src="<?php the_field('wcf-img'); ?>" class="not-found__img"/> -->
-						<img src="<?php echo get_template_directory_uri(); ?>/src/images/not-found-logo.png"/>
+						<?php 
+							$image = get_field('404_img', 'option');
+							if( !empty( $image ) ): ?>
+								<img src="<?php echo esc_url($image['url']); ?>" alt="img"/>
+						<?php endif; ?>
 						<h1>404</h1>
 					</div>
 					<div class="not-found-content__text-part">
-						<h2>Йой...<br>Сторінку не знайдено</h2>
-						<p>Ми не змогли знайти сторінку, яку ви шукали. Поверніться, будь ласка, на головну сторінку та спробуйте ще раз</p>
-						<a href="<?php echo get_home_url("/");?>">На головну</a>
+						<h2><?php the_field('404_title', 'option') ?></h2>
+						<p><?php the_field('404_text', 'option') ?></p>
+						<a href="<?php echo get_home_url("/");?>"><?php the_field('404_text-link', 'option') ?></a>
 					</div>
 				</div>
 			</div>
