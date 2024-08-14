e0a2345c7c31bcd3349600dc1f00f0f7b8c74228
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-08-14 10:10:09 +0300

    Created basic markup for 404 not-found page

--- 404.php
+++ 404.php
@@ -10,8 +10,22 @@
 get_header();
 ?>
 
-	<main id="primary">
-		<section>
+	<main id="primary" class="not-found">
+		<section class="section not-found-section">
+			<div class="container">
+				<div class="not-found-content">
+					<div class="not-found-content__img-part">
+						<!-- <img src="<?php the_field('wcf-img'); ?>" class="not-found__img"/> -->
+						<img src="<?php echo get_template_directory_uri(); ?>/src/images/not-found-logo.png"/>
+						<h1>404</h1>
+					</div>
+					<div class="not-found-content__text-part">
+						<h2>Йой...<br>Сторінку не знайдено</h2>
+						<p>Ми не змогли знайти сторінку, яку ви шукали. Поверніться, будь ласка, на головну сторінку та спробуйте ще раз</p>
+						<a href="<?php echo get_home_url("/");?>">На головну</a>
+					</div>
+				</div>
+			</div>
 		</section>
 	</main>
 
