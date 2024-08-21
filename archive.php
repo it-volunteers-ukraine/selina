6bdbb6abf4bd2546b3b59ea0c3cb5d5e073928bb
Oksana Baranik <ksyubaranik@gmail.com>
2024-08-21 20:12:15 +0200

    Made all posts shown if click on menu btn Archive, made filter for posts by tags

--- archive.php
+++ archive.php
@@ -20,6 +20,56 @@ get_header();
                 </div>
             </div>    
         </section>
+        <section class="tags-section section">
+            <div class="container">
+                
+                <ul class="tags-section__list">
+                    <?php
+                    // Отримання всіх тегів з таксономії 'news_tag'
+                    $terms = get_terms(array(
+                        'taxonomy'   => 'news_tag',
+                        'hide_empty' => true, // Показувати лише ті теги, які прив'язані до постів
+                    ));
+
+                    if (!empty($terms) && !is_wp_error($terms)) {
+                        foreach ($terms as $term) {
+                            // Отримання URL для фільтрації постів по тегу
+                            $term_link = get_term_link($term);
+                            echo '<li><a href="' . esc_url($term_link) . '">' . esc_html($term->name) . '</a></li>';
+                        }
+                    } else {
+                        echo '<li>Немає тегів.</li>';
+                    }
+                    ?>
+                </ul>
+                
+            </div>
+        </section>
+        <section class="news-section section">
+            <div class="container">
+                <div class="news-section__wrapper">
+
+                    <?php if ( have_posts() ) : ?>
+                        <?php while ( have_posts() ) : the_post(); ?>
+                            <article id="post-<?php the_ID(); ?>" class="news-section__post">
+                                <?php get_template_part('template-parts/one-card-news') ?>
+                                
+                                <!-- <div class="entry-meta">
+                                    <span class="tags">
+                                        <?php echo get_the_term_list( get_the_ID(), 'news_tag', 'Теги: ', ', ' ); // Теги поста ?>
+                                    </span>
+                                </div> -->
+                            </article>
+                        <?php endwhile; ?>
+
+                        <?php the_posts_pagination(); ?>
+
+                    <?php else : ?>
+                        <p>Немає записів для цього архіву.</p>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </section>
         <?php get_template_part('template-parts/join-us') ?>
     </main>
 
