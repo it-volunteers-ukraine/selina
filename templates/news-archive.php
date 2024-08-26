d291283182e7cea7114ac7bf9d113a4667cf0e4e
Oksana Baranik <ksyubaranik@gmail.com>
2024-08-26 19:49:05 +0200

    Added the ability for admin to change the color of tags

--- templates/news-archive.php
+++ templates/news-archive.php
@@ -25,7 +25,7 @@ get_header();
     <section class="tags-section section">
         <div class="container">
             <div class="filter-buttons">
-                <a href="<?php echo esc_url(get_permalink()); ?>" class="<?php echo (!isset($_GET['filter_tag']) || empty($_GET['filter_tag'])) ? 'active' : ''; ?>"><?php the_field('word_all'); ?></a>
+                <a href="<?php echo esc_url(get_permalink()); ?>" class="<?php echo (!isset($_GET['filter_tag']) || empty($_GET['filter_tag'])) ? 'active' : ''; ?> word_all-btn"><?php the_field('word_all'); ?></a>
 
                 <?php
                 // Get all tags from the `news_tag` taxonomy
@@ -37,27 +37,24 @@ get_header();
                 // Get active tags from the GET parameter
                 $active_tags = isset($_GET['filter_tag']) ? (array)$_GET['filter_tag'] : array();
 
-                // Output buttons for each tag
                 foreach ($terms as $term) {
-                    // Check if the tag is active
                     $is_active = in_array($term->slug, $active_tags);
-
-                    // If the tag is active, create a link to remove it from the active tags
+                
+                    $term_color = get_field('tag_color', 'news_tag_' . $term->term_id);
+                    $term_color_style = $term_color ? 'style="background-color:' . esc_attr($term_color) . ';"' : '';
+                
                     if ($is_active) {
                         $new_tags = array_diff($active_tags, array($term->slug));
                     } else {
-                        // If the tag is inactive, add it to the list of active tags
                         $new_tags = array_merge($active_tags, array($term->slug));
                     }
-
-                    // Create a URL with the new set of active tags
                     $new_url = esc_url(add_query_arg('filter_tag', $new_tags ? $new_tags : null));
-
-                    // Add the 'active' class to the active tag
+                
                     $active_class = $is_active ? 'active' : '';
-
-                    echo '<a href="' . $new_url . '" class="' . $active_class . '">' . esc_html($term->name) . '</a>';
+                
+                    echo '<a href="' . $new_url . '" class="' . $active_class . '" ' . $term_color_style . '>' . esc_html($term->name) . '</a>';
                 }
+                
                 ?>
             </div>
         </div>
