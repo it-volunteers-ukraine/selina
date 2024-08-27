fa8776d597fb2be1a286287a569e3189b81ce7d1
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-08-27 17:40:28 +0300

    Created sample pages

---
+++ templates/single-courses.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template post type: courses
+ * Template name: courses
+ */
+get_header();
+?>
+<main>
+    <section class="section">
+            <div class="container">
+            </div>
+    </section>
+
+</main>
+<?php get_footer(); ?>
