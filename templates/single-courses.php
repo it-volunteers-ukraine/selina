a5e0109b8ee48f33dd7269680ac5e5907c63f24e
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-08-27 18:54:46 +0300

    Updated courses

--- templates/single-courses.php
+++ templates/single-courses.php
@@ -8,7 +8,7 @@ get_header();
 <main>
     <section class="section">
             <div class="container">
-                нова сторінка
+               Приклад сторінки <h1><?php the_title(); ?></h1>
             </div>
     </section>
 
