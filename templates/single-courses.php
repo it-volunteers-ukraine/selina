934717980ffeff7f6ec83b9bec9be2b6f6928ec2
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-08-28 11:56:46 +0300

    Added top-course-section

--- templates/single-courses.php
+++ templates/single-courses.php
@@ -5,12 +5,39 @@
  */
 get_header();
 ?>
-<main>
-    <section class="section">
-            <div class="container">
-               Приклад сторінки <h1 class="single-courses-title"><?php the_title(); ?></h1>
+
+<main class="course">
+    <section class="top-course-section">
+        <style>
+            @media screen and (min-width: 1439px) {
+                .top-course-section {
+                    background-image: url("<?php the_field('upper-section__background', 'option') ?>");
+                }
+            }
+        </style>
+
+        <div class="container">
+            <h2 class="section_heading top-course-section_heading"><?php the_title() ?></h2>
+            <div class="sub-titles">
+                <a class="sub-title primary" href="#">
+                    <svg class="icon-paw" width="18" height="16">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-paw">
+                        </use>
+                    </svg>
+                    Курси
+                </a>
+                <a class="sub-title primary" href="#">
+                    <svg class="icon-paw" width="18" height="16">
+                        <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#icon-paw">
+                        </use>
+                    </svg>
+                    Анатомія
+                </a>
             </div>
+        </div>
     </section>
 
+    <?php get_template_part('template-parts/join-us') ?>
 </main>
+
 <?php get_footer(); ?>
