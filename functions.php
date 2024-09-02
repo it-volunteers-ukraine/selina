89ed53d41feb5eae8606d4477bffc3f687b027bf
Iryna <Iryna@MacBook-Air.local>
2024-09-02 15:16:21 +0300

    deleted functions

--- functions.php
+++ functions.php
@@ -647,8 +647,6 @@ add_action('wp_ajax_load_news_archive', 'load_news_archive');
 add_action('wp_ajax_nopriv_load_news_archive', 'load_news_archive');
 
 
-
-
 // Перенаправлення за сторінок входу, наприклад, з http://yoursite.com/wp-login.php
  /*
 function custom_login() {
