15ca0970c93a5bbeb346f2ab14600277c1288acf
hettfield git <olekpl90@gmail.com>
2024-09-02 15:12:53 +0200

    fix: fixed link in one card news

--- template-parts/one-card-news.php
+++ template-parts/one-card-news.php
@@ -10,7 +10,7 @@
             <?php the_field('news_text'); ?>
         </p>
     </div>
-    <a class="one-card-news__news-section__button news-section__button button red_medium_button" href="<?php the_field('news_link'); ?>">
+    <a class="one-card-news__news-section__button news-section__button button red_medium_button" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
         <?php the_field('news_btn'); ?>
         <svg class="news-section__button-svg" width="16" height="15">
             <use href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-paw"></use>
