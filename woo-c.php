bb1c4f282dbc950d38c6e3344537a2d87a879508
Iryna <Iryna@MacBook-Air.local>
2024-09-23 18:02:49 +0300

    basic functions

--- woo-c.php
+++ woo-c.php
@@ -8,17 +8,54 @@ add_theme_support( 'wc-product-gallery-lightbox' );
 add_theme_support( 'wc-product-gallery-slider' );
 }
 
-add_theme_support( 'woocommerce', array(
-    'single_image_width' => 600,
-    'thumbnail_image_width' => 600,
-    'gallery_thumbnail_image_width' => 60,
-) );
+function selina_add_woocommerce_support() {
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 150,
+        'single_image_width'    => 300,
 
-add_filter('woocommerce_get_image_size_single', function ($size){
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 1,
+            'max_columns'     => 5,
+        ),
+    ) );
+}
+
+add_action( 'after_setup_theme', 'selina_add_woocommerce_support' );
+
+// register ajax header cart
+function update_cart_count() {
+}
+add_action( 'wp_ajax_update_cart_count', 'update_cart_count' );
+add_action( 'wp_ajax_nopriv_update_cart_count', 'update_cart_count' );
+
+//==============images size =====================//
+// Adjust image size for single product pages
+add_filter('woocommerce_get_image_size_single', function($size) {
     return array(
-        'width' => 400,
-        'height' => 600,
-        'crop'   => 1,
+        'width'  => 500,
+        'height' => 500, 
+        'crop'   => 0 
     );
 });
 
+// Adjust image size for shop (product archive) pages
+add_filter('woocommerce_get_image_size_shop_catalog', function($size) {
+    return array(
+        'width'  => 300,
+        'height' => 300,
+        'crop'   => 0
+    );
+});
+
+// Adjust image size for related products and thumbnails
+add_filter('woocommerce_get_image_size_thumbnail', function($size) {
+    return array(
+        'width'  => 300,
+        'height' => 300,
+        'crop'   => 0
+    );
+});
