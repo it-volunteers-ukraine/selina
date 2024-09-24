09b4a8691f3647d123f36ad2e8e627e024862b1f
kuharenko-olya <olyavolk49@gmail.com>
2024-09-24 09:47:43 +0300

    add link

--- search.php
+++ search.php
@@ -10,8 +10,9 @@
             </p>
             <?php while (have_posts()) : the_post(); ?>
                 <div class="searched-results">
-                    <a class="searched" href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                    <a class="searched"><?php the_title() ?></a>
                     <p class="searched-description"><?php the_field('description') ?></p>
+                    <a style="color: #000" class="searched" href="<?php the_permalink() ?>"><?php the_field('search-link-read-more','option') ?></a>
                 </div>
             <?php endwhile; ?>
             <?php else : ?>
