8f7ec7004006bef66c087a745a75ba5e78596431
Oksana <ksyubaranik@gmail.com>
2024-10-01 19:02:01 +0200

    Made wrapper for cart

--- page-cart.php
+++ page-cart.php
@@ -2,6 +2,10 @@
 
 <?php if ( have_posts() ) : 
     while ( have_posts() ) : the_post(); ?>
+        <h1 style="font-weight: 800;
+                    font-size: 45px;
+                    line-height: 100%;
+                    color: #121212;"><?php the_title(); ?></h1>
         <?php the_content(); ?>
     <?php endwhile; 
 else : ?>
--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -18,6 +18,79 @@
 defined( 'ABSPATH' ) || exit;
 
 do_action( 'woocommerce_before_main_content' );
+
 ?>
+
+<div class="mywoo_before_cart">
+	<?php do_action( 'woocommerce_before_cart' ); ?>
+</div>
+
+<div class="mywoo-cart-form">
+	<form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
+		<?php do_action( 'woocommerce_before_cart_table' ); ?>
+
+			<table class="mywoo-cart-table">
+				<thead>
+					<tr>
+						<th class="product-thumbnail"><span class="screen-reader-text"><?php esc_html_e( 'Thumbnail image', 'woocommerce' ); ?></span></th>
+						<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
+						<th class="product-quantity"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
+						<th class="product-price"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
+						<th class="product-subtotal"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+						<th class="product-remove"><span class="screen-reader-text"><?php esc_html_e( 'Remove item', 'woocommerce' ); ?></span></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php do_action( 'woocommerce_before_cart_contents' ); ?>
+
+					<?php
+						foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+							$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+							$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+							/**
+							 * Filter the product name.
+							 *
+							 * @since 2.1.0
+							 * @param string $product_name Name of the product in the cart.
+							 * @param array $cart_item The product in the cart.
+							 * @param string $cart_item_key Key for the product in the cart.
+							 */
+							$product_name = apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key );
+
+							if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+								$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+
+							?>
+
+							<tr>
+								<!-- Product image -->
+								<td class="product-thumbnail">
+									<?php
+									$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+
+									if ( ! $product_permalink ) {
+										echo $thumbnail; // PHPCS: XSS ok.
+									} else {
+										printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
+									}
+									?>
+								</td>
+
+							</tr>
+
+
+
+							<?php
+						}
+					}
+					?>
+
+					<?php do_action( 'woocommerce_after_cart_contents' ); ?>
+				</tbody>
+			</table>
+
+		<?php do_action( 'woocommerce_after_cart_table' ); ?>
+	</form>
+</div>
 
 <?php do_action( 'woocommerce_after_main_content' ); ?>
