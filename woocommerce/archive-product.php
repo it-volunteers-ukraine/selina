9387cf5968feeea3d9beb0979c828db1a6780826
Iryna <Iryna@MacBook-Air.local>
2024-10-05 16:29:48 +0300

    catalog added

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,97 +1,67 @@
 <?php
-/**
- * The Template for displaying product archives, including the main shop page which is a post type archive
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 8.6.0
- */
-
-defined( 'ABSPATH' ) || exit;
-
-get_header( 'shop' );
-
-/**
- * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
- */
-do_action( 'woocommerce_before_main_content' );
-
-/**
- * Hook: woocommerce_shop_loop_header.
- *
- * @since 8.6.0
- *
- * @hooked woocommerce_product_taxonomy_archive_header - 10
- */
-do_action( 'woocommerce_shop_loop_header' );
-
-if ( woocommerce_product_loop() ) {
-
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
-
-	woocommerce_product_loop_start();
-
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-			the_post();
-
-			/**
-			 * Hook: woocommerce_shop_loop.
-			 */
-			do_action( 'woocommerce_shop_loop' );
-
-			wc_get_template_part( 'content', 'product' );
-		}
-	}
-
-	woocommerce_product_loop_end();
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
+// Ensure WooCommerce is loaded
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
 }
 
-/**
- * Hook: woocommerce_after_main_content.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
-do_action( 'woocommerce_after_main_content' );
+get_header( 'shop' ); ?>
 
-/**
- * Hook: woocommerce_sidebar.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action( 'woocommerce_sidebar' );
+<div class="shop-page">
 
-get_footer( 'shop' );
+    <!-- Filters Section -->
+    <div class="shop-filters">
+        <div class="shop-filters-categories">
+            <?php 
+                // WooCommerce product categories filter
+                wp_list_categories( array(
+                    'taxonomy'     => 'product_cat',
+                    'title_li'     => '',
+                    'show_count'   => true,
+                    'hide_empty'   => true,
+                    'walker'       => new Walker_Category(), // Optional if you need a custom walker
+                ) ); 
+            ?>
+        </div>
+
+        <div class="shop-filters-products">
+            <button>Товари</button>
+            <button>Послуги</button>
+            <button>Цифрова Продукція</button>
+        </div>
+    </div>
+
+    <!-- Page Description -->
+    <div class="shop-page-description">
+        <p>В цьому розділі зібрано все необхідне для вашого котика: товари, послуги та цифрова продукція...</p>
+    </div>
+
+    <!-- Sorting Section -->
+    <div class="shop-sorting">
+        <span><?php woocommerce_catalog_ordering(); ?></span>
+    </div>
+
+    <!-- Product Grid -->
+    <div class="products-grid">
+        <?php if ( woocommerce_product_loop() ) : ?>
+
+            <?php woocommerce_product_loop_start(); ?>
+
+            <?php while ( have_posts() ) : ?>
+                <?php the_post(); ?>
+
+                <div class="product-item">
+                    <?php wc_get_template_part( 'content', 'product' ); ?>
+                </div>
+
+            <?php endwhile; ?>
+
+            <?php woocommerce_product_loop_end(); ?>
+
+        <?php else : ?>
+            <?php wc_get_template( 'loop/no-products-found.php' ); ?>
+        <?php endif; ?>
+    </div>
+
+</div>
+
+<?php get_footer( 'shop' ); ?>
