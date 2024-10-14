5509f80bb5b7ea9cdede680d58b14363f54003af
hettfield git <olekpl90@gmail.com>
2024-10-14 11:03:11 +0200

    flex container

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -33,7 +33,7 @@ if ( post_password_required() ) {
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 	<div class="single-product__flex-container">
-		<div>
+		<div class='single-product__image-sharing-container'>
 			<?php
 			/**
 			 * Hook: woocommerce_before_single_product_summary.
