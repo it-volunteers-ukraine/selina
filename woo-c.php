4bf4d101b01143f38c7b417b7f9154f40d74d8a4
hettfield git <olekpl90@gmail.com>
2024-11-26 10:37:14 +0100

    fix some styles in cart table

--- woo-c.php
+++ woo-c.php
@@ -427,30 +427,30 @@ function customize_checkout_fields( $fields ) {
     return $fields;
 }
 
-echo '<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const deliveryOptions = document.querySelectorAll(".delivery-pickup, .delivery-nova-poshta");
+// echo '<script>
+//     document.addEventListener("DOMContentLoaded", function () {
+//         const deliveryOptions = document.querySelectorAll(".delivery-pickup, .delivery-nova-poshta");
 
-        deliveryOptions.forEach(option => {
-            option.addEventListener("click", function () {
-                deliveryOptions.forEach(opt => opt.classList.remove("selected"));
-                this.classList.add("selected");
-            });
-        });
-    });
-</script>';
+//         deliveryOptions.forEach(option => {
+//             option.addEventListener("click", function () {
+//                 deliveryOptions.forEach(opt => opt.classList.remove("selected"));
+//                 this.classList.add("selected");
+//             });
+//         });
+//     });
+// </script>';
 
-echo '<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const deliveryOptions = document.querySelectorAll(".payment-1, .payment-2, .payment-3, .payment-4");
+// echo '<script>
+//     document.addEventListener("DOMContentLoaded", function () {
+//         const deliveryOptions = document.querySelectorAll(".payment-1, .payment-2, .payment-3, .payment-4");
 
-        deliveryOptions.forEach(option => {
-            option.addEventListener("click", function () {
-                deliveryOptions.forEach(opt => opt.classList.remove("selected"));
-                this.classList.add("selected");
-            });
-        });
-    });
-</script>';
+//         deliveryOptions.forEach(option => {
+//             option.addEventListener("click", function () {
+//                 deliveryOptions.forEach(opt => opt.classList.remove("selected"));
+//                 this.classList.add("selected");
+//             });
+//         });
+//     });
+// </script>';
 
 
