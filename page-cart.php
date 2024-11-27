6880102a08324ade3ea6f115e31a1e972d5ce0e5
Oksana <ksyubaranik@gmail.com>
2024-11-27 10:40:01 +0100

    Made backup of the stage, create dinamic link for Continue Shopping according to the selected language

--- page-cart.php
+++ page-cart.php
@@ -14,11 +14,27 @@
             
             <div class="container cart-header__container">
                 <h1><?php the_title(); ?></h1>
-                <?php if ( ! WC()->cart->is_empty() ) : ?>
+                <!-- <?php if ( ! WC()->cart->is_empty() ) : ?>
                     <a href="<?php echo esc_url( wc_get_page_permalink( 'shop' ) ); ?>">
                         <?php echo esc_html( get_field( 'continue_shopping', get_the_ID() ) ); ?>
                     </a>
+                <?php endif; ?> -->
+                <?php if ( ! WC()->cart->is_empty() ) : ?>
+                    <?php 
+                        // Get the ID of the shop page
+                        $shop_page_id = wc_get_page_id( 'shop' );
+
+                        // Get the ID of the translated version of the shop page for the current language
+                        $translated_shop_page_id = pll_get_post( $shop_page_id );
+
+                        // Get the URL of the translated shop page
+                        $shop_url = get_permalink( $translated_shop_page_id );
+                    ?>
+                    <a href="<?php echo esc_url( $shop_url ); ?>">
+                        <?php echo esc_html( get_field( 'continue_shopping', get_the_ID() ) ); ?>
+                    </a>
                 <?php endif; ?>
+
             </div>
             
             <?php the_content(); ?>
